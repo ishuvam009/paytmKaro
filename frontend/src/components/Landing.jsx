7c9276aaaf0eadfea4d1df334304b69685282be7
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-09-05 00:27:03 +0530

    Commit

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Landing() {
   return (
     <>
-      <div className="text-center ">HI</div>
+      <div className="flex justify-center mt-10">
+      <div className="text-center">HI Join Toay Paytm,</div>
+      <Link to={"/signup"}>Click Here</Link>
+      </div>
     </>
   );
 }
